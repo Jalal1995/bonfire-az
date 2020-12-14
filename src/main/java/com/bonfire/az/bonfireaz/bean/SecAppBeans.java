d8fe9bd3a7e875fbe0bff64dd5a44537dd8c7193
ibcelal@gmail.com <ibcelalh>
2020-12-14 18:44:20 +0400

    -- cors

--- src/main/java/com/bonfire/az/bonfireaz/bean/SecAppBeans.java
+++ src/main/java/com/bonfire/az/bonfireaz/bean/SecAppBeans.java
@@ -1,9 +1,13 @@
 package com.bonfire.az.bonfireaz.bean;
 
+import com.google.common.collect.ImmutableList;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecAppBeans {
@@ -11,6 +15,22 @@ public class SecAppBeans {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(ImmutableList.of("https://www.yourdomain.com")); // www - obligatory
+//        configuration.setAllowedOrigins(ImmutableList.of("*"));  //set access from all domains
+        configuration.setAllowedMethods(ImmutableList.of("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(ImmutableList.of("Authorization", "Cache-Control", "Content-Type"));
+
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 
 }
--- src/main/java/com/bonfire/az/bonfireaz/conf/MySecurityConfig.java
+++ src/main/java/com/bonfire/az/bonfireaz/conf/MySecurityConfig.java
@@ -25,6 +25,7 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .cors().and()
                 .csrf().disable();
 
         http
