30008cfc7bb163aa418cd10e2a0a98527350ee76
ibcelal@gmail.com <ibcelalh>
2020-12-14 18:47:32 +0400

    -- all domains

--- src/main/java/com/bonfire/az/bonfireaz/bean/SecAppBeans.java
+++ src/main/java/com/bonfire/az/bonfireaz/bean/SecAppBeans.java
@@ -21,8 +21,8 @@ public class SecAppBeans {
     public CorsConfigurationSource corsConfigurationSource() {
         final CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(ImmutableList.of("https://www.yourdomain.com")); // www - obligatory
-//        configuration.setAllowedOrigins(ImmutableList.of("*"));  //set access from all domains
+//        configuration.setAllowedOrigins(ImmutableList.of("https://www.yourdomain.com")); // www - obligatory
+        configuration.setAllowedOrigins(ImmutableList.of("*"));  //set access from all domains
         configuration.setAllowedMethods(ImmutableList.of("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowCredentials(true);
         configuration.setAllowedHeaders(ImmutableList.of("Authorization", "Cache-Control", "Content-Type"));
