6e09029c4a3afa1956f541bd38e2e9acc0c38084
ibcelal@gmail.com <ibcelalh>
2020-12-15 12:44:19 +0400

    -- cors

--- src/main/java/com/bonfire/az/bonfireaz/conf/WebConfiguration.java
+++ src/main/java/com/bonfire/az/bonfireaz/conf/WebConfiguration.java
@@ -1,5 +1,6 @@
 package com.bonfire.az.bonfireaz.conf;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,7 +10,18 @@ public class WebConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
+        registry.addMapping("/*")
                 .allowedMethods("*");
+    }
+
+    @Bean
+    public WebMvcConfigurer configure() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/*")
+                        .allowedOrigins("*");
+            }
+        };
     }
 }
